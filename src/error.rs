ad380028610f77a591234f982c670c1845bbd9ec
Ivan Akimov <ivan@fossfox.com>
2024-06-30 16:50:12 -0500

    wip

--- src/error.rs
+++ src/error.rs
@@ -4,28 +4,28 @@ use thiserror::Error;
 
 #[derive(Error, Debug, Eq, PartialEq)]
 pub enum Error {
-    #[error("Min length has to be between 0 and 255")]
-    MinLengthRange,
-    #[error("Numbers cannot be negative")]
-    NegativeNumbers,
+	#[error("Min length has to be between 0 and 255")]
+	MinLengthRange,
+	#[error("Numbers cannot be negative")]
+	NegativeNumbers,
 }
 
 pub enum PgError {
-    SqidsError(sqids::Error),
-    CustomError(Error),
+	SqidsError(sqids::Error),
+	CustomError(Error),
 }
 
 impl fmt::Display for PgError {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            PgError::SqidsError(err) => error!("{}", err),
-            PgError::CustomError(err) => error!("{}", err),
-        }
-    }
+	fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			PgError::SqidsError(err) => error!("{}", err),
+			PgError::CustomError(err) => error!("{}", err),
+		}
+	}
 }
 
 impl From<sqids::Error> for PgError {
-    fn from(err: sqids::Error) -> Self {
-        PgError::SqidsError(err)
-    }
+	fn from(err: sqids::Error) -> Self {
+		PgError::SqidsError(err)
+	}
 }
